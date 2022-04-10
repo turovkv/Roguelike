2323182747fd44f79dedabba8017b960f7ab6786
kirill <turovkv@yandex.ru>
2022-04-10 19:04:03 +0300

    Update logic 2

--- roguelike/src/main/kotlin/ru/roguelike/logic/LogicFacade.kt
+++ roguelike/src/main/kotlin/ru/roguelike/logic/LogicFacade.kt
@@ -1,10 +1,6 @@
 package ru.roguelike.logic
 
-import com.googlecode.lanterna.screen.Screen
-import ru.roguelike.model.Character
-
 class LogicFacade(
-    private val character: Character,
     private val map: MapLogic,
     private val instructions: InstructionsLogic,
     private var current: Logic = map,
