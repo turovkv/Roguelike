d4e8136f1881ad212c3dc2f3969e5e182f70c1a5
kirill <turovkv@yandex.ru>
2022-04-10 22:20:55 +0300

    Add comments

--- roguelike/src/main/kotlin/ru/roguelike/logic/Logic.kt
+++ roguelike/src/main/kotlin/ru/roguelike/logic/Logic.kt
@@ -1,9 +1,28 @@
 package ru.roguelike.logic
 
 interface Logic {
+    /*
+    * обработка движения влево
+    * */
     fun processLeftArrow()
+
+    /*
+    * обработка движения вправо
+    * */
     fun processRightArrow()
+
+    /*
+    * обработка движения вверх
+    * */
     fun processUpArrow()
+
+    /*
+    * обработка движения вниз
+    * */
     fun processDownArrow()
+
+    /*
+    * отрисовать текущую логику
+    * */
     fun draw()
 }
--- roguelike/src/main/kotlin/ru/roguelike/logic/LogicFacade.kt
+++ roguelike/src/main/kotlin/ru/roguelike/logic/LogicFacade.kt
@@ -5,15 +5,38 @@ class LogicFacade(
     private val instructions: InstructionsLogic,
     private var current: Logic = map,
 ) {
+
+    /*
+    * обработка движения влево
+    * */
     fun processLeftArrow() = current.processLeftArrow()
+
+    /*
+    * обработка движения вправо
+    * */
     fun processRightArrow() = current.processRightArrow()
+
+    /*
+    * обработка движения вверх
+    * */
     fun processUpArrow() = current.processUpArrow()
+
+    /*
+    * обработка движения вниз
+    * */
     fun processDownArrow() = current.processDownArrow()
+
+    /*
+    * сменить текущую логику на instructions и нарисовать ее
+    * */
     fun processHelp() {
         current = instructions
         current.draw()
     }
 
+    /*
+    * сменить текущую логику на map и нарисовать ее
+    * */
     fun processMap() {
         current = map
         current.draw()
