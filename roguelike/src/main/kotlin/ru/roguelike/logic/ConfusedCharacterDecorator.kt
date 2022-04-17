0e4640e467d3e1729513e6a7a994e1ad97f7b908
Andrey Shein <andrey.shein.spb@gmail.com>
2022-04-17 21:18:56 +0300

    confused character decorator

--- roguelike/src/main/kotlin/ru/roguelike/logic/ConfusedCharacterDecorator.kt
+++ roguelike/src/main/kotlin/ru/roguelike/logic/ConfusedCharacterDecorator.kt
@@ -1,12 +1,19 @@
 package ru.roguelike.logic
 
 import ru.roguelike.model.Coordinates
+import kotlin.random.Random
 
 class ConfusedCharacterDecorator(
     private val mapLogic: MapLogic,
     private val characterStrategy: CharacterStrategy
 ) : CharacterStrategy(mapLogic = mapLogic) {
     override fun move(coordinates: Coordinates): Coordinates {
-        TODO("Not yet implemented")
+        val possible = arrayOf(
+            Coordinates(coordinates.x - 1, coordinates.y),
+            Coordinates(coordinates.x, coordinates.y - 1),
+            Coordinates(coordinates.x, coordinates.y + 1),
+            Coordinates(coordinates.x + 1, coordinates.y),
+        )
+        return possible[Random.nextInt(possible.size)]
     }
 }
