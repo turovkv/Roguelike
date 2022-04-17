4fdb93feeabba1967f7e3b0d8173fd962eeb81b7
Andrey Shein <andrey.shein.spb@gmail.com>
2022-04-17 20:46:04 +0300

    add map logic to character strategy

--- roguelike/src/main/kotlin/ru/roguelike/logic/CharacterStrategy.kt
+++ roguelike/src/main/kotlin/ru/roguelike/logic/CharacterStrategy.kt
@@ -2,6 +2,8 @@ package ru.roguelike.logic
 
 import ru.roguelike.model.Coordinates
 
-interface CharacterStrategy {
-    fun move(coordinates: Coordinates): Coordinates
+abstract class CharacterStrategy (
+    mapLogic: MapLogic
+) {
+    abstract fun move(coordinates: Coordinates): Coordinates
 }
