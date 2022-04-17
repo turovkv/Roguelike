c56d10f245cb86219548d716eefd728e4add4c2e
Kirill Turov <turovkv@yandex.ru>
2022-04-17 21:38:29 +0300

    Add isVisible function to MapLogic

--- roguelike/src/main/kotlin/ru/roguelike/logic/MapLogic.kt
+++ roguelike/src/main/kotlin/ru/roguelike/logic/MapLogic.kt
@@ -4,6 +4,8 @@ import ru.roguelike.model.Character
 import ru.roguelike.model.Coordinates
 import ru.roguelike.model.MapModel
 import ru.roguelike.view.Drawable
+import kotlin.math.max
+import kotlin.math.min
 
 class MapLogic(
     private val character: Character,
@@ -13,6 +15,73 @@ class MapLogic(
 
     private fun checkCoordinates(c: Coordinates): Boolean {
         return 0 <= c.x && c.x < mapModel.getX() && 0 <= c.y && c.y < mapModel.getY() && mapModel.isWalkable(c)
+    }
+
+    fun isVisible(c1: Coordinates, c2: Coordinates): Boolean {
+        if (c1 == c2) {
+            return true
+        }
+
+        if (c1.x == c2.x) {
+            for (y in min(c1.y, c2.y)..max(c1.y, c2.y)) {
+                if (!checkCoordinates(Coordinates(c1.x, y))) {
+                    return false
+                }
+            }
+            return true
+        }
+
+        if (c1.y == c2.y) {
+            for (x in min(c1.x, c2.x)..max(c1.x, c2.x)) {
+                if (!checkCoordinates(Coordinates(x, c1.y))) {
+                    return false
+                }
+            }
+            return true
+        }
+
+        // c1.x != c2.x && c1.y != c2.y
+        for (x in min(c1.x, c2.x) until max(c1.x, c2.x)) {
+            val y = (x - c1.x) * (c2.y - c1.y) * 1.0 / (c2.x - c1.x) + c1.y
+            if (!checkCoordinates(Coordinates(x - 1, y.toInt()))) {
+                return false
+            }
+            if (!checkCoordinates(Coordinates(x, y.toInt()))) {
+                return false
+            }
+            if (y.toInt().toDouble() == y) {
+                if (!checkCoordinates(Coordinates(x - 1, y.toInt() - 1))) {
+                    return false
+                }
+                if (!checkCoordinates(Coordinates(x, y.toInt() - 1))) {
+                    return false
+                }
+            }
+        }
+
+        for (y in min(c1.y, c2.y) until max(c1.y, c2.y)) {
+            val x = (y - c1.y) * (c2.x - c1.x) * 1.0 / (c2.y - c1.y) + c1.x
+            if (!checkCoordinates(Coordinates(x.toInt(), y - 1))) {
+                return false
+            }
+            if (!checkCoordinates(Coordinates(x.toInt(), y))) {
+                return false
+            }
+            if (x.toInt().toDouble() == x) {
+                if (!checkCoordinates(Coordinates(x.toInt() - 1, y - 1))) {
+                    return false
+                }
+                if (!checkCoordinates(Coordinates(x.toInt() - 1, y))) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    fun updateNPCs() {
+
     }
 
     /**
