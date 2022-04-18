5092af7d9350725f00d76f18f0952624adfccfe3
Victor Samoilov <victor.samoilov.2002@mail.ru>
2022-04-18 22:14:50 +0300

    Add util for generators

--- roguelike/src/main/kotlin/ru/roguelike/model/generator/TextFileFieldGenerator.kt
+++ roguelike/src/main/kotlin/ru/roguelike/model/generator/TextFileFieldGenerator.kt
@@ -1,10 +1,6 @@
 package ru.roguelike.model.generator
 
-import ru.roguelike.model.Cell
-import ru.roguelike.model.CellType
 import ru.roguelike.model.Field
-import ru.roguelike.view.NON_WALKABLE_CHAR
-import ru.roguelike.view.WALKABLE_CHAR
 import java.io.FileInputStream
 
 /**
@@ -13,16 +9,6 @@ import java.io.FileInputStream
 class TextFileFieldGenerator(private val path: String) : FieldGenerator {
     override fun generate(): Field =
         FileInputStream(path).use { input ->
-            input.bufferedReader().use { reader ->
-                generateSequence(reader::readLine).map { line ->
-                    line.map { char ->
-                        when (char) {
-                            NON_WALKABLE_CHAR -> Cell(CellType.NON_WALKABLE)
-                            WALKABLE_CHAR -> Cell(CellType.WALKABLE)
-                            else -> error("Unsupported char")
-                        }
-                    }.toMutableList()
-                }.toMutableList()
-            }
+            generateFromInputStream(input)
         }
 }
---
+++ roguelike/src/main/kotlin/ru/roguelike/model/generator/util.kt
@@ -0,0 +1,46 @@
+package ru.roguelike.model.generator
+
+import ru.roguelike.logic.AgressiveStrategy
+import ru.roguelike.logic.PassiveStrategy
+import ru.roguelike.logic.SneakyStrategy
+import ru.roguelike.model.Apple
+import ru.roguelike.model.Cell
+import ru.roguelike.model.CellType
+import ru.roguelike.model.Coordinates
+import ru.roguelike.model.Enemy
+import ru.roguelike.model.Shield
+import ru.roguelike.model.Sword
+import ru.roguelike.util.Constants
+import ru.roguelike.view.AGRESSIVE_CHAR
+import ru.roguelike.view.APPLE_CHAR
+import ru.roguelike.view.NON_WALKABLE_CHAR
+import ru.roguelike.view.PASSIVE_CHAR
+import ru.roguelike.view.SHIELD_CHAR
+import ru.roguelike.view.SNEAKY_CHAR
+import ru.roguelike.view.SWORD_CHAR
+import ru.roguelike.view.WALKABLE_CHAR
+import java.io.InputStream
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+fun Char.toCell(c: Coordinates): Cell =
+    when (this) {
+        NON_WALKABLE_CHAR -> Cell(CellType.NON_WALKABLE)
+        WALKABLE_CHAR -> Cell(CellType.WALKABLE)
+        SWORD_CHAR -> Cell(CellType.WALKABLE, Sword(Random.nextInt(1..Constants.MAX_DAMAGE)))
+        APPLE_CHAR -> Cell(CellType.WALKABLE, Apple(Random.nextInt(1..Constants.MAX_HP)))
+        SHIELD_CHAR -> Cell(CellType.WALKABLE, Shield(Random.nextInt(1..Constants.MAX_ARMOR)))
+        AGRESSIVE_CHAR -> Cell(CellType.WALKABLE, null, Enemy(c, Random.nextInt(1..Constants.MAX_HP), AgressiveStrategy()))
+        PASSIVE_CHAR -> Cell(CellType.WALKABLE, null, Enemy(c, Random.nextInt(1..Constants.MAX_HP), PassiveStrategy()))
+        SNEAKY_CHAR -> Cell(CellType.WALKABLE, null, Enemy(c, Random.nextInt(1..Constants.MAX_HP), SneakyStrategy()))
+        else -> error("unsupported character")
+    }
+
+fun generateFromInputStream(input: InputStream) =
+    input.bufferedReader().use { reader ->
+        generateSequence(reader::readLine).mapIndexed { row, line ->
+            line.mapIndexed { column, char ->
+                char.toCell(Coordinates(column, row))
+            }.toMutableList()
+        }.toMutableList()
+    }
