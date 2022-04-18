adac1ec53948da1f5e155983fb7faa38b4d909bc
Victor Samoilov <victor.samoilov.2002@mail.ru>
2022-04-18 21:09:49 +0300

    Change val on vars

--- roguelike/src/main/kotlin/ru/roguelike/util/Constants.kt
+++ roguelike/src/main/kotlin/ru/roguelike/util/Constants.kt
@@ -4,20 +4,20 @@ package ru.roguelike.util
  * Class that stores project constants
  */
 object Constants {
-    const val FIELD_WIDTH = 100
-    const val FIELD_HEIGHT = 30
-    const val CHARACTER_VIEW_HEIGHT = 4
-    const val ERROR_VIEW_HEIGHT = 1
+    var FIELD_WIDTH = 100
+    var FIELD_HEIGHT = 30
+    var CHARACTER_VIEW_HEIGHT = 4
+    var ERROR_VIEW_HEIGHT = 1
 
-    const val MIN_CELL_SIZE = 10
-    const val MAX_CELL_SIZE = 15
-    const val ROOM_SIZE = 4
+    var MIN_CELL_SIZE = 10
+    var MAX_CELL_SIZE = 15
+    var ROOM_SIZE = 4
 
-    const val INVENTORY_SIZE = 5
-    const val MAXIMUM_EQUIPPED_ITEMS = 2
+    var INVENTORY_SIZE = 5
+    var MAXIMUM_EQUIPPED_ITEMS = 2
 
-    const val ROOM_ENEMY_COUNT = 2
-    const val ROOM_ITEMS_COUNT = 3
+    var ROOM_ENEMY_COUNT = 2
+    var ROOM_ITEMS_COUNT = 3
 
     const val MAX_HP = 70
     const val MAX_DAMAGE = 30
