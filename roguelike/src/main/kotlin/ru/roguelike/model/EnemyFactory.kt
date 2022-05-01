701e9b2972f3cf0ce63c368f5bedbdcc82153857
Kirill Turov <turovkv@yandex.ru>
2022-05-01 20:32:18 +0300

    Add comments

--- roguelike/src/main/kotlin/ru/roguelike/model/EnemyFactory.kt
+++ roguelike/src/main/kotlin/ru/roguelike/model/EnemyFactory.kt
@@ -4,10 +4,22 @@ package ru.roguelike.model
  * EnemyFactory which can create dragons, skeletons, cyborgs
  * */
 interface EnemyFactory {
+    /**
+     * create dragon :)
+     * */
     fun createDragon(x: Int, y: Int): Enemy
+    /**
+     * create skeleton :)
+     * */
     fun createSkeleton(x: Int, y: Int): Enemy
+    /**
+     * create cyborg :)
+     * */
     fun createCyborg(x: Int, y: Int): Enemy
 
+    /**
+     * create cyborg or skeleton or dragon randomly :)
+     * */
     fun createRandomEnemy(x: Int, y: Int): Enemy =
         when (EnemyStyle.values().random()) {
             EnemyStyle.DRAGON -> createDragon(x, y)
