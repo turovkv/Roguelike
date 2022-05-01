38a2f33a5e1cc34d90b7b2ce72cd9cd7411f3331
Victor Samoilov <victor.samoilov.2002@mail.ru>
2022-05-01 21:09:30 +0300

    Fix test

--- roguelike/src/test/kotlin/ru/roguelike/model/generator/bsp/BuildFielderTest.kt
+++ roguelike/src/test/kotlin/ru/roguelike/model/generator/bsp/BuildFielderTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 import ru.roguelike.model.EnemyFactoryImpl
 import ru.roguelike.model.generator.FieldBuilder
 import java.io.FileInputStream
-import java.util.*
+import java.util.Properties
 
 class BuildFielderTest {
     @Test
@@ -22,9 +22,11 @@ class BuildFielderTest {
 
     @Test
     fun propertiesTest() {
-        val field = FieldBuilder(EnemyFactoryImpl()).withProperties(Properties().also {
-            it.load(FileInputStream("src/test/resources/map.properties"))
-        }).build()
+        val field = FieldBuilder(EnemyFactoryImpl()).withProperties(
+            Properties().also {
+                it.load(FileInputStream("src/test/resources/map.properties"))
+            }
+        ).build()
 
         Assertions.assertEquals(1000, field[0].size)
         Assertions.assertEquals(1000, field.size)
