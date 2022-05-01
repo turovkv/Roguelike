9a2e2073987fa3456758451c4736c4319b9d3a8c
Victor Samoilov <victor.samoilov.2002@mail.ru>
2022-05-01 20:21:21 +0300

    Add comments to builder

--- roguelike/src/main/kotlin/ru/roguelike/model/generator/FieldBuilder.kt
+++ roguelike/src/main/kotlin/ru/roguelike/model/generator/FieldBuilder.kt
@@ -6,17 +6,26 @@ import ru.roguelike.model.generator.bsp.BSPFieldGenerator
 import ru.roguelike.util.Constants
 import java.util.Properties
 
+/**
+ * Specify format of file with field
+ */
 enum class FormatType {
     JSON,
     PLAIN
 }
 
+/**
+ * Builder for field
+ */
 class FieldBuilder(private val factory: EnemyFactory) {
     private var width = Constants.FIELD_WIDTH
     private var height = Constants.FIELD_HEIGHT
     private var filePath: String? = null
     private var fileFormatType: FormatType = FormatType.JSON
 
+    /**
+     * Creates field with parameters, which were specified earlier
+     */
     fun build(): Field =
         if (filePath == null) {
             BSPFieldGenerator(factory).generate()
@@ -29,26 +38,41 @@ class FieldBuilder(private val factory: EnemyFactory) {
             } ?: throw IllegalStateException("File path is not specified")
         }
 
+    /**
+     * Specify width of field
+     */
     fun withWidth(width: Int): FieldBuilder {
         this.width = width
         return this
     }
 
+    /**
+     * Specify height of field
+     */
     fun withHeight(height: Int): FieldBuilder {
         this.height = height
         return this
     }
 
+    /**
+     * Specify path to file, from which we want to read our field
+     */
     fun fromFile(path: String): FieldBuilder {
         filePath = path
         return this
     }
 
+    /**
+     * Specify format of file, from which we want to read our field
+     */
     fun withFormat(formatType: FormatType): FieldBuilder {
         fileFormatType = formatType
         return this
     }
 
+    /**
+     * Specify parameters from properties
+     */
     fun withProperties(prop: Properties): FieldBuilder {
         prop.getProperty("width")?.toInt()?.let { withWidth(it) }
         prop.getProperty("height")?.toInt()?.let { withHeight(it) }
